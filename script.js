8c85f039025e4ab90d33012ee6a8de6dbd676688
ashurbz <rbzashu1@gmail.com>
2020-10-09 21:26:07 +0530

    added comments

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let hours = document.getElementById('hours');
-let mins = document.getElementById('mins');
-let seconds = document.getElementById('seconds');
+let hours = document.getElementById('hours');   // hours span element from html
+let mins = document.getElementById('mins');    // mins span element from html
+let seconds = document.getElementById('seconds'); // seconds span element from html
 
- document.getElementById("date").innerText=new Date().toDateString();
-setInterval(()=>{hours.innerText=new Date().getHours()+" Hrs"});
-setInterval(()=>{mins.innerText=new Date().getMinutes()+" Mins"});
-setInterval(()=>{seconds.innerText=new Date().getSeconds()+" Secs"});
+document.getElementById("date").innerText = new Date().toDateString();   // Getting date from pre define function
+setInterval(() => { hours.innerText = new Date().getHours() + " Hrs" });  // getting hours from date function
+setInterval(() => { mins.innerText = new Date().getMinutes() + " Mins" });  // getting minutes from date function
+setInterval(() => { seconds.innerText = new Date().getSeconds() + " Secs" }); // getting seconds from date function
